ad4020d8cbd096d1fe59680bf257d76407136b4b
fabio <faavendano0@soy.sena.edu.co>
2024-04-05 22:59:17 -0500

    se le quita contraseña y se agrega ruta public

--- api/config/config.php
+++ api/config/config.php
@@ -7,7 +7,7 @@ require_once __DIR__ .'/../../vendor/autoload.php';
 $host = 'localhost';
 $dbname = 'mi_api_rest';
 $username = 'root';
-$password = '90112276040Aa*';
+$password = '';
 
 // Establece la conexion a la base de datos
 \RedBeanPHP\R::setup('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
